02f92926084af46e7a2667dffa6f2c4de0a91c24
spectrespectre0 <williamadt123@gmail.com>
2024-10-17 18:11:07 +0700

    feat: Main Menu GUI

--- matrixApp/src/main/java/app/matrixapp/HelloController.java
+++ matrixApp/src/main/java/app/matrixapp/HelloController.java
@@ -6,25 +6,10 @@ import javafx.scene.control.Label;
 import matrix.*;
 
 public class HelloController {
-    public Matrix matrix = new Matrix(3,3);
-    public double determinant = matrix.determinant();
-    public Button counterButton;
-    private int clickCount = 0;
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    private Label counterLabel;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!"+determinant);
-    }
 
-    @FXML
-    protected void onCounterButtonClick() {
-        clickCount++;
-        counterLabel.setText("Click Count : "+clickCount);
-    }
+
+
 
 }
